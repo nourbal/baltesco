02a8322c6d0424c08797a3a1264ba7c712d8961d
Noureddine <noureddine.ballouki@gmail.com>
2021-01-29 01:29:03 +0100

    HomePage
    integration of home page

---
+++ storage/cms/cache/83/d7/page-daccueil.htm.php
@@ -0,0 +1,5 @@
+<?php 
+class Cms60135152bd3e0730441618_08dd9f20ff511b82734882399b22b546Class extends Cms\Classes\PageCode
+{
+
+}
---
+++ storage/cms/twig/60/60e2a982977198d238692bd4040e1ae476fb560793b9acd2e99f9594f7480b36.php
@@ -0,0 +1,79 @@
+<?php
+
+use Twig\Environment;
+use Twig\Error\LoaderError;
+use Twig\Error\RuntimeError;
+use Twig\Extension\SandboxExtension;
+use Twig\Markup;
+use Twig\Sandbox\SecurityError;
+use Twig\Sandbox\SecurityNotAllowedTagError;
+use Twig\Sandbox\SecurityNotAllowedFilterError;
+use Twig\Sandbox\SecurityNotAllowedFunctionError;
+use Twig\Source;
+use Twig\Template;
+
+/* C:\laragon\www\baltesco/themes/baltesco/layouts/fallback.htm */
+class __TwigTemplate_c85f8d8c018343890241a54b3a19fd342607dd8af31a54709f72d2485b8c417a extends \Twig\Template
+{
+    private $source;
+    private $macros = [];
+
+    public function __construct(Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->source = $this->getSourceContext();
+
+        $this->parent = false;
+
+        $this->blocks = [
+        ];
+        $this->sandbox = $this->env->getExtension('\Twig\Extension\SandboxExtension');
+        $tags = array("page" => 1);
+        $filters = array();
+        $functions = array();
+
+        try {
+            $this->sandbox->checkSecurity(
+                ['page'],
+                [],
+                []
+            );
+        } catch (SecurityError $e) {
+            $e->setSourceContext($this->source);
+
+            if ($e instanceof SecurityNotAllowedTagError && isset($tags[$e->getTagName()])) {
+                $e->setTemplateLine($tags[$e->getTagName()]);
+            } elseif ($e instanceof SecurityNotAllowedFilterError && isset($filters[$e->getFilterName()])) {
+                $e->setTemplateLine($filters[$e->getFilterName()]);
+            } elseif ($e instanceof SecurityNotAllowedFunctionError && isset($functions[$e->getFunctionName()])) {
+                $e->setTemplateLine($functions[$e->getFunctionName()]);
+            }
+
+            throw $e;
+        }
+
+    }
+
+    protected function doDisplay(array $context, array $blocks = [])
+    {
+        $macros = $this->macros;
+        // line 1
+        echo $this->env->getExtension('Cms\Twig\Extension')->pageFunction();
+    }
+
+    public function getTemplateName()
+    {
+        return "C:\\laragon\\www\\baltesco/themes/baltesco/layouts/fallback.htm";
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  62 => 1,);
+    }
+
+    public function getSourceContext()
+    {
+        return new Source("{% page %}", "C:\\laragon\\www\\baltesco/themes/baltesco/layouts/fallback.htm", "");
+    }
+}
