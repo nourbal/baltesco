9386d73447c85362a5d9b978eb91270c79a399ac
Noureddine <noureddine.ballouki@gmail.com>
2021-02-07 04:03:35 +0100

    gitflow-feature-stash: backend
    add plugins baltesco 
    to customize website

---
+++ plugins/nourbal/baltesco/models/Link.php
@@ -0,0 +1,27 @@
+<?php namespace Nourbal\Baltesco\Models;
+
+use Model;
+
+/**
+ * Model
+ */
+class Link extends Model
+{
+    use \October\Rain\Database\Traits\Validation;
+    
+    use \October\Rain\Database\Traits\SoftDelete;
+
+    protected $dates = ['deleted_at'];
+
+
+    /**
+     * @var string The database table used by the model.
+     */
+    public $table = 'nourbal_baltesco_links';
+
+    /**
+     * @var array Validation rules
+     */
+    public $rules = [
+    ];
+}
